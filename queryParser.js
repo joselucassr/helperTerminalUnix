1f13a5f6dd94a370a1047591bfd72b6b2d0ed177
José Lucas R. Santos <joselucasrs@gmail.com>
2022-02-17 16:13:36 -0300

    Nomes com aspas agora podem ser copiados. Mudança dos commits para português.

--- queryParser.js
+++ queryParser.js
@@ -91,7 +91,7 @@ const writeHtml = (queryResults) => {
     <div id="${qr.id}" class="card">
       <div class="cardFirstLine">
         <div class="purpleHighlight">~$ ${qr.name}</div>
-        <div onclick="writeToClipboard('${qr.name}', '${qr.id}')" style="width: 25px" class="pointer"><img src="copySvg.svg" alt="" /></div>
+        <div onclick="writeToClipboard(&#96;${qr.name}&#96;, '${qr.id}')" style="width: 25px" class="pointer"><img src="copySvg.svg" alt="" /></div>
       </div>
       <div>
         <div class="commentHightlight"># Basicamente</div>
